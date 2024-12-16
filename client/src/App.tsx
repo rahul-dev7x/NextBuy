ba40a30be527ce15e586f23603c2b767420cea71
rahul-dev7x <rahulbera9932@gmail.com>
2024-12-16 06:07:10 +0530

    feat: integrate react-router for navigation and update App component structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
+import { Outlet } from "react-router-dom"
+import Header from "./components/shared/Header"
+import Footer from "./components/shared/Footer"
 
 
 const App = () => {
   return (
+    <>
     <div>
-      
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
     </div>
+    </>
   )
 }
 
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,16 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { persistor, store } from './redux/store.ts'
 import { PersistGate } from 'redux-persist/integration/react'
+import { RouterProvider } from 'react-router-dom'
+import router from './routes/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
   <PersistGate loading={null} persistor={persistor}>
-    <App />
+    <RouterProvider router={router}/>
   </PersistGate>
 </Provider>
 )
